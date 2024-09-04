20acf258a716fdba45e9547710da288d86106fa8
mahinvora <mahinsabbirhusen@gmail.com>
2024-09-04 20:55:01 +0530

    added restaurants page

--- delish-dine/src/App.js
+++ delish-dine/src/App.js
@@ -5,6 +5,10 @@ import SignUp from "./Pages/SignUp/SignUp";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import Cart from "./Pages/Cart/Cart";
 import Home from "./Pages/Home/Home";
+import Restaurants from "./Pages/Restaurants/Restaurants";
+import RestaurantDetail from "./Pages/RestaurantDetail/RestaurantDetail";
+import Booking from "./Pages/Booking/Booking";
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -14,6 +18,16 @@ export default function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/home/:id" element={<Home></Home>}></Route>
+        <Route
+          exact
+          path="/restaurants/:username"
+          element={<Restaurants></Restaurants>}
+        ></Route>
+        <Route
+          path="/restaurants/:name/:username"
+          element={<RestaurantDetail></RestaurantDetail>}
+        />
+        <Route path="/booking/:name/:username" element={<Booking></Booking>} />
       </Routes>
     </BrowserRouter>
   );
