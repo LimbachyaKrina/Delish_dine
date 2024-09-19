bc9ce68324790228c358cd5bfb7b00a26bade5c2
LimbachiyaKrina <limbachiyakrina157@gmail.com>
2024-09-19 12:24:17 +0530

    css change

--- delish-dine/src/Components/Navbar/Navbar.js
+++ delish-dine/src/Components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link,useParams } from 'react-router-dom';
 import { faShoppingCart, faUser, faUtensils, faInfoCircle, faCalendarCheck, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { name,id } = useParams();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -27,34 +28,34 @@ const Navbar = () => {
         </div>
         <ul className="menu-list">
           <li>
-            <a href="/restaurant" className="nav-link">
+            <a href="/restaurant" className="nav-link navbarLinks">
               <FontAwesomeIcon icon={faUtensils} className="mr-2" />
               Restaurants
             </a>
           </li>
           <li>
-            <a href="/bookings" className="nav-link">
+            <a href="/bookings" className="nav-link navbarLinks">
               <FontAwesomeIcon icon={faCalendarCheck} className="mr-2" />
               Bookings
             </a>
           </li>
           <li>
-            <a href="/about-us" className="nav-link">
+            <a href="/about-us" className="nav-link navbarLinks">
               <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
               About Us
             </a>
           </li>
         </ul>
         <div className="notifications">
-        <Link to="/cart" className="nav-link">
-        <FontAwesomeIcon icon={faShoppingCart} size="lg" color='#2F7CD5'/>
+        <Link to={`/cart/${id}`} className="nav-link  ">
+        <FontAwesomeIcon icon={faShoppingCart} size="lg" color='#fff'/>
         <span className="notification-count">0</span>
         <span className="icon-text" >Cart</span>
           </Link>          
         </div>
         <div className="user-profile">
-        <Link to="/profile" className="nav-link">
-          <FontAwesomeIcon icon={faUser} className="fa-user" size="lg" color='#2F7CD5'/>
+        <Link to="/profile" className="nav-link ">
+          <FontAwesomeIcon icon={faUser} className="fa-user" size="lg" color='#fff'/>
           <span className="icon-text" >Profile</span>
         </Link>
       </div>
